7f7d5c382e1bae53df64f5df80fa0d0a83b75dec
JustAGuy <jehannabar68@gmail.com>
2026-09-17 00:07:07 +0800

    feat: added post and delete endpoints

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ app.get('/api/products', async (req: Request, res: Response) => {
     try {
         // Returns an array of Row Data Packets
         const [results] = await db.query(
-            `SELECT * FROM PRODUCTS`
+            `SELECT * FROM products`
         );
 
         if (!Array.isArray(results) || results.length === 0) {
@@ -48,6 +48,89 @@ app.post('/api/products', async (req: Request, res: Response) => {
         );
 
         res.status(201).json(paginatedResults);
+    }
+    catch {
+        res.status(500).json({ message: "Get request for products failed"})
+    }
+})
+
+app.get('/api/products/:id', async (req: Request, res: Response) => {
+    try {
+        const productId = req.params.id;
+        const product = await db.query(
+            `SELECT * FROM products WHERE id = ?`,
+            [productId]
+        );
+
+        if (!product) {
+            res.status(404).json({ error: 'Product not found.' });
+            return;
+        }
+
+        res.status(200).json(product);
+    }
+    catch {
+        res.status(500).json({ message: "Get request for products failed"})
+    }
+})
+
+app.put('/api/products/:id', async (req: Request, res: Response) => {
+    try {
+        const productId = req.params.id;
+        const body = req.body; 
+        const { name, price, stock, category, size, isAvailable } = body            // Destructure
+
+        if (price <= 0) {
+            res.status(400).json({ error: 'Price must be a positive number.' });
+            return;
+        }
+        if (stock < 0) {
+            res.status(400).json({ error: 'Stock cannot be negative.' });
+            return;
+        }
+
+        const updated_details: string[] = [];
+
+        for (const [key, value] of Object.entries(body)) {
+            updated_details.push(`${key} = ?`);
+        } 
+
+        const updated_obj = await db.query(
+            `UPDATE products
+            SET ${updated_details.join(', ')}
+            WHERE id = ?`,
+            [productId]
+        );
+
+        if (!updated_obj) {
+            res.status(404).json({ error: 'Product was unable to be updated.' });
+            return;
+        }
+
+        const new_object = await db.query(`SELECT * FROM products WHERE id = ?`, [productId]);
+        res.status(200).json(new_object);
+    }
+    catch {
+        res.status(500).json({ message: "Get request for products failed"})
+    }
+})
+
+app.delete('/api/products/:id', async (req: Request, res: Response) => {
+    try {
+        const productId = req.params.id;
+        const deleted_obj = await db.query(
+            `DELETE 
+            FROM products
+            WHERE id = ?`,
+            [productId]
+        );
+
+        if (!deleted_obj) {
+            res.status(404).json({ error: 'Product not found or is null.' });
+            return;
+        }
+
+        res.status(200).json({message: "Product deleted successfully"});
     }
     catch {
         res.status(500).json({ message: "Get request for products failed"})
