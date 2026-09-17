0ac1ac2b86f766033691f51800de06e3c6a0a38b
JehannAbar <jehannabar68@gmail.com>
2026-09-17 23:19:27 +0800

    docs/fix: fixed code issues and added README.md

--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
 });
 
 export default db;
